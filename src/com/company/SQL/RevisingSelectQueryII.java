88b9c3cefe71c8fabf7b557cfd0dc2a3da1a4f31
Matthew Greco <mgreco2112@tutanota.com>
2022-08-17 12:22:33 -0400

    finished RevisingSelectQueryII

---
+++ src/com/company/SQL/RevisingSelectQueryII.java
@@ -0,0 +1,6 @@
+package com.company.SQL;
+
+public class RevisingSelectQueryII {
+//    SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
+}
+//REMINDER!!! Querying String data here only uses single quotes
