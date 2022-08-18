9e290c529599bf5cf0e389bc274aee6623d577af
Matthew Greco <mgreco2112@tutanota.com>
2022-08-18 01:12:55 -0400

    finished Weather Obsesrvation Station

---
+++ src/com/company/SQL/WeatherObservationStation.java
@@ -0,0 +1,7 @@
+package com.company.SQL;
+
+public class WeatherObservationStation {
+//    SELECT CITY, STATE FROM STATION;
+}
+
+//REMINDER!!! Selecting multiple columns is denoted by a comma between column names
