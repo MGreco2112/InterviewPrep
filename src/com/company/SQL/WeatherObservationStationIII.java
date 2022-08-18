89e83350b7712b4483ccfe3f1b013ae4f48df1e9
Matthew Greco <mgreco2112@tutanota.com>
2022-08-18 09:27:29 -0400

    finished Weather Obs III

---
+++ src/com/company/SQL/WeatherObservationStationIII.java
@@ -0,0 +1,7 @@
+package com.company.SQL;
+
+public class WeatherObservationStationIII {
+//    SELECT DISTINCT CITY FROM STATION WHERE MOD(ID,2) = 0;
+}
+
+//REMINDER!!! DISTINCT returns a unique set of data, MOD(x,y) returns the result of a Modulus function
