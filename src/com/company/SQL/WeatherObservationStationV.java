318e91feefed2defe6d38c4752e75d1200a978a5
Matthew Greco <mgreco2112@tutanota.com>
2022-08-20 13:46:26 -0400

    finished WeatherObservationStationV with Richies help

---
+++ src/com/company/SQL/WeatherObservationStationV.java
@@ -0,0 +1,15 @@
+package com.company.SQL;
+
+public class WhetherObservationStationV {
+//SELECT CITY, LENGTH(CITY) FROM STATION
+//WHERE LENGTH(CITY) IN
+//(SELECT MIN(LENGTH(CITY)) FROM STATION)
+//ORDER BY CITY ASC LIMIT 1;
+//
+//SELECT CITY, LENGTH(CITY) FROM STATION
+//WHERE LENGTH(CITY) IN
+//(SELECT MAX(LENGTH(CITY)) FROM STATION)
+//ORDER BY CITY ASC LIMIT 1;
+}
+//REMINDER!!! The subquery runs first, then the where condition is compared to the return of the subquery
+//this is return is then formatted into the select fields and the order by and limit clauses are applied as filters
