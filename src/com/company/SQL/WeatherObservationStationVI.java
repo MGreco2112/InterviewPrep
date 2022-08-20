b7d6ef288ab75a2c5b4e8155c383f9484559081e
Matthew Greco <mgreco2112@tutanota.com>
2022-08-20 14:08:29 -0400

    finished WeatherObservationStationVI

---
+++ src/com/company/SQL/WeatherObservationStationVI.java
@@ -0,0 +1,5 @@
+package com.company.SQL;
+
+public class WeatherObservationStationVI {
+//    SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE "A%" OR CITY LIKE "E%" OR CITY LIKE "I%" OR CITY LIKE "O%" OR CITY LIKE "U%";
+}
