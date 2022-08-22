129ac205e54e8b3c76301a9679b8e592309981f3
Matthew Greco <mgreco2112@tutanota.com>
2022-08-22 19:01:57 -0400

    finished CamelCaseFour

--- src/com/company/ThreeMonthChallenge/CamelCaseFour.java
+++ src/com/company/ThreeMonthChallenge/CamelCaseFour.java
@@ -6,27 +6,28 @@ public class CamelCaseFour {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scanner = new Scanner(System.in);
-        boolean isSplit = false;
+        boolean isSplit;
         String inputType = "";
         int postHeaderIndex = 4;
 
         while (scanner.hasNext()) {
 
-
             String input = scanner.nextLine();
 
             StringBuilder output = new StringBuilder();
 
-            if (input.substring(0, 1).equals("S")) {
+            if (input.charAt(0) == 'S') {
                 isSplit = true;
+            } else {
+                isSplit = false;
             }
 
             inputType = input.substring(2, 3);
 
-            if (inputType.equals("C")) {
+            if (inputType.equals("C") && !isSplit) {
                 output.append(Character.toUpperCase(input.charAt(postHeaderIndex)));
             } else {
-                output.append(input.charAt(postHeaderIndex));
+                output.append(Character.toLowerCase(input.charAt(postHeaderIndex)));
             }
 
             if (isSplit) {
@@ -43,11 +44,22 @@ public class CamelCaseFour {
                 }
             } else {
                 for (int i = postHeaderIndex + 1; i < input.length(); i++) {
-                    //TODO write logic for combining
+                    if (input.charAt(i) == ' ') {
+                        output.append(Character.toUpperCase(input.charAt(i+1)));
+                        i++;
+                    } else {
+                        output.append(input.charAt(i));
+                    }
+                }
+
+                if (inputType.equals("M")) {
+                    output.append("(").append(")");
                 }
             }
 
             System.out.println(output);
         }
+
+        scanner.close();
     }
 }
