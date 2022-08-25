375fe40cd3904117658eb1b16499e4b183a0444d
Matthew Greco <mgreco2112@tutanota.com>
2022-08-25 13:10:25 -0400

    finished TypeOfTriangle

---
+++ src/com/company/SQL/TypeOfTriangle.java
@@ -0,0 +1,12 @@
+package com.company.SQL;
+
+public class TypeOfTriangle {
+//    SELECT CASE
+//    WHEN A + B <= C THEN "Not A Triangle"
+//    WHEN A = B AND B = C THEN "Equilateral"
+//    WHEN A = B OR B = C OR A = C THEN "Isosceles"
+//    WHEN NOT A = B OR NOT B = C OR NOT C = A THEN "Scalene"
+//    END
+//FROM triangles;
+//
+}
