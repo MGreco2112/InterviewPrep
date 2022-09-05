e9fdbac1fc1b83731d2cfb59442d012220731cdd
Matthew Greco <mgreco2112@tutanota.com>
2022-09-05 19:11:26 -0400

    added description comment, will refine solution after reading instructions again

--- src/com/company/ThreeMonthChallenge/ConnectedGroups.java
+++ src/com/company/ThreeMonthChallenge/ConnectedGroups.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public class ConnectedGroups {
     public static int[] solution(List<List<Integer>> input) {
+        //This method creates an array with each index referencing one person
+        //each index holds a number which says how many other people have a connection with that person
         int[] output = new int[input.size()];
 
         for (int i = 0; i < input.size(); i++) {
