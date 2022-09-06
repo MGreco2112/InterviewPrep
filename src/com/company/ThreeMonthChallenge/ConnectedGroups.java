99d3190f163fa60d2158eb371286252fc91b14e0
Matthew Greco <mgreco2112@tutanota.com>
2022-09-06 11:16:27 -0400

    working solution attempt 2

--- src/com/company/ThreeMonthChallenge/ConnectedGroups.java
+++ src/com/company/ThreeMonthChallenge/ConnectedGroups.java
@@ -2,10 +2,11 @@ package com.company.ThreeMonthChallenge;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ConnectedGroups {
-    public static int[] solution(List<List<Integer>> input) {
+    public static int solution(List<List<Integer>> input) {
         //This method creates an array with each index referencing one person
         //each index holds a number which says how many other people have a connection with that person
         int[] output = new int[input.size()];
@@ -18,6 +19,12 @@ public class ConnectedGroups {
             }
         }
 
-        return output;
+        List<Integer> formattedOutput = new ArrayList<>();
+
+        for (int connection : output) {
+            formattedOutput.add(connection);
+        }
+
+        return Collections.max(formattedOutput) - 1;
     }
 }
