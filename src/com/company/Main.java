1c9003c2225c949f295fc802040280ca9491ac98
Matthew Greco <mgreco2112@tutanota.com>
2022-09-06 12:37:07 -0400

    removed ConnectedGroups, wrong repository

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,8 @@
 package com.company;
 
-import com.company.ThreeMonthChallenge.ConnectedGroups;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-        List<List<Integer>> outerList = new ArrayList<>();
-        List<Integer> listA = Arrays.asList(1,1,0,0);
-        List<Integer> listB = Arrays.asList(1,1,1,0);
-        List<Integer> listC = Arrays.asList(0,1,1,0);
-        List<Integer> listD = Arrays.asList(0,0,0,1);
 
-        outerList.add(listA);
-        outerList.add(listB);
-        outerList.add(listC);
-        outerList.add(listD);
-
-        System.out.println(ConnectedGroups.solution(outerList));
     }
 }
