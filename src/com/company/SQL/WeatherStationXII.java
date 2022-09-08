94f32680d9f458b30fa44dbbd534497f940f7560
Matthew Greco <mgreco2112@tutanota.com>
2022-09-08 14:31:06 -0400

    finished WeatherStationXIII with Travis

---
+++ src/com/company/SQL/WeatherStationXII.java
@@ -0,0 +1,7 @@
+package com.company.SQL;
+
+public class WeatherStationXII {
+//    -- SELECT TRUNCATE(SUM(lat_n),4) FROM STATION WHERE lat_n > 38.7880 AND lat_n < 137.2345;
+//
+//    -- SELECT TRUNCATE(SUM(lat_n), 4) FROM STATION WHERE lat_n BETWEEN 38.7881 AND 137.2344;
+}
