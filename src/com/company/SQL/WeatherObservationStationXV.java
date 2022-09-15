eef3bdbbb71324c89f5c9d2e1c0e380942ecfeca
Matthew Greco <mgreco2112@tutanota.com>
2022-09-15 19:15:24 -0400

    finished WeatherObservationStationXV

---
+++ src/com/company/SQL/WeatherObservationStationXV.java
@@ -0,0 +1,8 @@
+package com.company.SQL;
+
+public class WeatherObservationStationXV {
+//SELECT ROUND(long_w, 4)
+//FROM STATION WHERE lat_n
+//IN
+//(SELECT MAX(lat_n) FROM STATION WHERE lat_n < 137.2345);
+}
