ab1c0c023c35589442c971b3260943d899a63a2a
Matthew Greco <mgreco2112@tutanota.com>
2022-09-17 00:40:13 -0400

    finished ThePads

---
+++ src/com/company/SQL/ThePads.java
@@ -0,0 +1,9 @@
+package com.company.SQL;
+
+public class ThePads {
+//SELECT CONCAT(name, "(", SUBSTR(occupation, 1, 1), ")") FROM occupations
+//ORDER BY name ASC;
+
+//SELECT CONCAT("There are a total of ", COUNT(occupation), " ", LOWER(occupation), "s.") FROM occupations
+//GROUP BY occupation ORDER BY COUNT(occupation) ASC;
+}
