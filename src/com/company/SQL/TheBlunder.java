018d5e951cc3411e0677e2c8aaab8c86bce4294f
Matthew Greco <mgreco2112@tutanota.com>
2022-09-22 16:48:39 -0400

    finished TheBlunder

---
+++ src/com/company/SQL/TheBlunder.java
@@ -0,0 +1,9 @@
+package com.company.SQL;
+
+public class TheBlunder {
+//-- SELECT ROUND(SUM(salary) / COUNT(*)) FROM employees;
+//
+//-- SELECT ROUND(SUM(REPLACE(salary, 0, "")) / COUNT(*)) FROM employees;
+//
+//SELECT ROUND(SUM(salary) / COUNT(*)) - ROUND(SUM(REPLACE(salary, 0, "")) / COUNT(*)) FROM employees;
+}
