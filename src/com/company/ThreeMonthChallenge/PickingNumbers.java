0efc03405decbc0df129824e83ba17b65bb9ee85
Matthew Greco <mgreco2112@tutanota.com>
2022-10-02 13:02:33 -0400

    modifying current solution to PickingNumbers. Can get one of two test cases passed, however fixing the code to pass one causes the other to currently fail

--- src/com/company/ThreeMonthChallenge/PickingNumbers.java
+++ src/com/company/ThreeMonthChallenge/PickingNumbers.java
@@ -6,17 +6,27 @@ import java.util.List;
 
 public class PickingNumbers {
     public static int pickingNumbers(List<Integer> a) {
+
         Collections.sort(a);
+
+        System.out.println(a);
 
         List<Integer> streaks = new ArrayList<>();
 
         int currentNum = a.get(0);
-        int currentStreak = 0;
+        int currentStreak = 1;
 
         for (int i = 1; i < a.size(); i++) {
-            if (a.get(i) > currentNum) {
+            System.out.println(a.get(i));
+            System.out.println("current num: " + currentNum);
+
+            if (a.get(i) >= currentNum + 1) {
+                System.out.println(true);
+
                 streaks.add(currentStreak);
-                currentStreak = 0;
+                currentStreak = 1;
+
+                currentNum = a.get(i);
             }
 
             if (a.get(i) == currentNum || a.get(i) == currentNum + 1) {
@@ -28,5 +38,4 @@ public class PickingNumbers {
 
         return Collections.max(streaks);
     }
-
 }
