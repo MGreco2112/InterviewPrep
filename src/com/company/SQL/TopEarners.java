598f24b613b78396a63b495fddf95bea86157e3f
Matthew Greco <mgreco2112@tutanota.com>
2022-10-05 17:30:27 -0400

    finished TopEarners with help

---
+++ src/com/company/SQL/TopEarners.java
@@ -0,0 +1,7 @@
+package com.company.SQL;
+
+public class TopEarners {
+//    SELECT months*salary, COUNT(*) FROM Employee GROUP BY 1 ORDER BY 1 DESC LIMIT 1;
+}
+//solution researched, as I couldn't understand how to get the max without referencing it prior to running call
+//will research group by and order by to better understand this solution as written
