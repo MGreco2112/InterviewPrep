c9c06adea13200548b8f4b66fed9ee7254b48357
Matthew Greco <mgreco2112@tutanota.com>
2022-10-14 09:30:11 -0400

    started formatting PermutingTwoArrays

---
+++ src/com/company/ThreeMonthChallenge/PermutingTwoArrays.java
@@ -0,0 +1,24 @@
+package com.company.ThreeMonthChallenge;
+
+import java.util.List;
+
+public class PermutingTwoArrays {
+    /*
+        There are two n-element arrays of integers, A and B. Permute them into some and such that the relation holds for all where
+        A[i] + B[i] >= K holds for all i where 0 <= n < K.
+        There will be n queries consisting of A, B, and K. For each query, return YES if some permutation A, B
+        satisfying the relation exists.
+        Otherwise, return NO.
+    */
+
+    public static String twoArrays(int k, List<Integer> A, List<Integer> B) {
+        // Write your code here
+
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
