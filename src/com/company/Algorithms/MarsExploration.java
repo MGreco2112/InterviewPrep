83ff5c2af11d64f4bbbd8ba76e197a977c29b93e
Matthew Greco <mgreco2112@tutanota.com>
2022-10-22 16:36:50 -0400

    finished MarsExploration

--- src/com/company/Algorithms/MarsExploration.java
+++ src/com/company/Algorithms/MarsExploration.java
@@ -1,4 +1,25 @@
 package com.company.Algorithms;
 
 public class MarsExploration {
+    public static int marsExploration(String s) {
+        int output = 0;
+
+        char[] message = {'S', 'O', 'S'};
+
+        int messageIndex = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != message[messageIndex]) {
+                output++;
+            }
+
+            messageIndex++;
+
+            if (messageIndex == message.length) {
+                messageIndex = 0;
+            }
+        }
+
+        return output;
+    }
 }
