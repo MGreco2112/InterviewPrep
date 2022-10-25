c5f9cb2d69353fb3124b1b35107dfea9e680e062
Matthew Greco <mgreco2112@tutanota.com>
2022-10-25 02:47:38 -0400

    tested with simple single test case, will refactor and build upon to pass more dynamic test cases

--- src/com/company/ThreeMonthChallenge/SeparateTheNumbers.java
+++ src/com/company/ThreeMonthChallenge/SeparateTheNumbers.java
@@ -1,4 +1,21 @@
 package com.company.ThreeMonthChallenge;
 
 public class SeparateTheNumbers {
+    public static void separateNumbers(String s) {
+
+        for (int i = 1; i < s.length(); i+= 2) {
+            if (Integer.parseInt(Character.toString(s.charAt(i))) - Integer.parseInt(Character.toString(s.charAt(i - 1))) != 1) {
+                System.out.println("NO");
+                return;
+            }
+        }
+
+        System.out.println("YES " + s.charAt(0));
+    }
+
+    public static void main(String[] args) {
+        separateNumbers("1234");
+
+        //tested with original test case, built to only pass original test case. Will refactor to pass more dynamically
+    }
 }
