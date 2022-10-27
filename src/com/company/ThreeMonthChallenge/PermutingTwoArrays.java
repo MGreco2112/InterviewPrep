d3e4f7a31eb10715526773b9e4661f28e6386ce7
Matthew Greco <mgreco2112@tutanota.com>
2022-10-27 13:42:57 -0400

    wrote basic iterative check without permutation system

--- src/com/company/ThreeMonthChallenge/PermutingTwoArrays.java
+++ src/com/company/ThreeMonthChallenge/PermutingTwoArrays.java
@@ -13,8 +13,17 @@ public class PermutingTwoArrays {
 
     public static String twoArrays(int k, List<Integer> A, List<Integer> B) {
         // Write your code here
+        //basic iteration without permutation system implemented
 
-        return "NO";
+
+        for (int i = 0; i < A.size(); i++) {
+            if (A.get(i) + B.get(i) <= k) {
+                return "NO";
+            }
+        }
+
+
+        return "YES";
     }
 
     public static void main(String[] args) {
