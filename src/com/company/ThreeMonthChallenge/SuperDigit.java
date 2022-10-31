58e181ec0771d014b7f77d705aee6ed7146f6bd9
Matthew Greco <mgreco2112@tutanota.com>
2022-10-31 16:32:02 -0400

    updated with researched solution, will ask for clarification on instructions

--- src/com/company/ThreeMonthChallenge/SuperDigit.java
+++ src/com/company/ThreeMonthChallenge/SuperDigit.java
@@ -20,4 +20,21 @@ public class SuperDigit {
 
     }
 
+    /*
+        public static int superDigit(String n, int k) {
+        if(n.length()==1 && k==1){
+            return Integer.parseInt(n);
+        } else {
+            long result=0;
+            for(int i=0; i<n.length(); i++){
+                result+=Character.getNumericValue(n.charAt(i));
+            }
+            result*=k;
+            return superDigit(result+"", 1);
+        }
+    }
+
+        researched solution
+     */
+
 }
