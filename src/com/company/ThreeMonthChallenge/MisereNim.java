587f6fa00d44249ec8ace9f6745086017a695f03
Matthew Greco <mgreco2112@tutanota.com>
2022-11-07 13:00:00 -0500

    modified soltuion for effeciency

--- src/com/company/ThreeMonthChallenge/MisereNim.java
+++ src/com/company/ThreeMonthChallenge/MisereNim.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class MisereNim {
     public static String misereNim(List<Integer> s) {
         // boolean isFirstPlayer = true;
-        int playerCount = 1;
+        int playerCount = 0;
 
         Map<Integer, Integer> stoneMap = new HashMap<>();
 
@@ -21,19 +21,23 @@ public class MisereNim {
         while (true) {
             boolean tookStone = false;
 
-            for (Integer index : stoneMap.keySet()) {
-                System.out.println(index);
+            List<Object> stoneMapKeys = Arrays.asList(stoneMap.keySet().toArray());
+
+            for (int i = 0; i < stoneMapKeys.size(); i++) {
+                System.out.println(stoneMapKeys.get(i));
                 playerCount++;
 
 
-                if (stoneMap.get(index) != 0) {
-                    int value = stoneMap.get(index);
+                if (stoneMap.get(stoneMapKeys.get(i)) != 0) {
+                    int value = stoneMap.get(stoneMapKeys.get(i));
                     value--;
-                    stoneMap.put(index, value);
+                    stoneMap.put((Integer) stoneMapKeys.get(i), value);
                     tookStone = true;
 
                     break;
 
+                } else {
+                    stoneMap.remove(stoneMapKeys.get(i));
                 }
 
             }
