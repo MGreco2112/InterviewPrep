bd71108bdabbb63ea76bb4a6c03b0d5e2ac36e68
Matthew Greco <mgreco2112@tutanota.com>
2022-11-09 15:29:10 -0500

    solved MaxPerimeterTriangle

--- src/com/company/ThreeMonthChallenge/MaxPerimiterTriangle.java
+++ src/com/company/ThreeMonthChallenge/MaxPerimiterTriangle.java
@@ -1,14 +1,38 @@
 package com.company.ThreeMonthChallenge;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MaxPerimiterTriangle {
 
     public static List<Integer> maximumPerimeterTriangle(List<Integer> sticks) {
-        // Write your code here
+        Collections.sort(sticks);
 
-        return null;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+        for (int i = 0; i < sticks.size() - 2; i++) {
+            int tempA = sticks.get(i);
+            int tempB = sticks.get(i + 1);
+            int tempC = sticks.get(i + 2);
+
+            if (tempA + tempB > tempC && tempB + tempC > tempA && tempA + tempC > tempB) {
+                a = tempA;
+                b = tempB;
+                c = tempC;
+            }
+        }
+
+        if (a == 0) {
+            return List.of(-1);
+        }
+
+
+
+        return Arrays.asList(a, b, c);
     }
 
 }
