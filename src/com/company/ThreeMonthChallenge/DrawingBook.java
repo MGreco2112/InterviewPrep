05f6ac97ad23734e82899abffb57a037374c18a5
Matthew Greco <mgreco2112@tutanota.com>
2022-11-10 10:04:30 -0500

    created potential solution, going to bug test

--- src/com/company/ThreeMonthChallenge/DrawingBook.java
+++ src/com/company/ThreeMonthChallenge/DrawingBook.java
@@ -2,6 +2,15 @@ package com.company.ThreeMonthChallenge;
 
 public class DrawingBook {
     public static int pageCount(int n, int p) {
-        return 0;
+        int forwardCount = p / 2;
+
+        int backwardCount = (n - p) / 2;
+
+        if (backwardCount == 1) {
+            backwardCount = n % 2 == 0 ? 1 : 0;
+        }
+
+
+        return Math.min(forwardCount, backwardCount);
     }
 }
