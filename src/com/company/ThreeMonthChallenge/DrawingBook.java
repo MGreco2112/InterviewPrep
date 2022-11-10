efbbcfa2c6f0ba1825808d18bfa661f90727722c
Matthew Greco <mgreco2112@tutanota.com>
2022-11-10 10:06:58 -0500

    basic testing looks promising, will send to HackerRank once connection is restored

--- src/com/company/ThreeMonthChallenge/DrawingBook.java
+++ src/com/company/ThreeMonthChallenge/DrawingBook.java
@@ -4,13 +4,19 @@ public class DrawingBook {
     public static int pageCount(int n, int p) {
         int forwardCount = p / 2;
 
-        int backwardCount = (n - p) / 2;
+        int backwardCount = n - p;
 
         if (backwardCount == 1) {
             backwardCount = n % 2 == 0 ? 1 : 0;
+        } else {
+            backwardCount = backwardCount / 2;
         }
 
 
         return Math.min(forwardCount, backwardCount);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pageCount(10, 8));
     }
 }
