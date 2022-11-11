e841102ace42c52156f80c0f7833121065d8028a
Matthew Greco <mgreco2112@tutanota.com>
2022-11-11 17:58:06 -0500

    solution started for TowerBreakers

--- src/com/company/ThreeMonthChallenge/TowerBreakers.java
+++ src/com/company/ThreeMonthChallenge/TowerBreakers.java
@@ -1,4 +1,29 @@
 package com.company.ThreeMonthChallenge;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TowerBreakers {
+    public static int towerBreakers(int n, int m) {
+        // Write your code here
+        int currentPlayer = 0;
+
+        Map<Integer, Integer> towerMap = towerMapBuilder(n, m);
+
+
+
+
+        return currentPlayer % 2 == 0 ? 1 : 2;
+    }
+
+    public static Map<Integer, Integer> towerMapBuilder(int n, int m) {
+        Map<Integer, Integer> towerMap = new HashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            towerMap.put(i, m);
+        }
+
+        return towerMap;
+    }
+
 }
