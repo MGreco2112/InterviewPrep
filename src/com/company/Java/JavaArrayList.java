0972366fd6a45bd61e72ab10e97f4cd760d17a92
Matthew Greco <mgreco2112@tutanota.com>
2023-01-14 12:41:35 -0500

    attempted solution for JavaArrayList. One case passed, going to refactor

--- src/com/company/Java/JavaArrayList.java
+++ src/com/company/Java/JavaArrayList.java
@@ -4,49 +4,59 @@ import java.util.*;
 
 public class JavaArrayList {
 
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner scanner = new Scanner(System.in);
-        List<List<Integer>> lines = new ArrayList<>();
 
-        int numberOfLines = scanner.nextInt();
+        int listLength = Integer.parseInt(scanner.nextLine());
 
-        scanner.nextLine();
+        List<String> listElements = new ArrayList<>();
 
-        while (numberOfLines > 0) {
-            List<Integer> newList = new ArrayList<>();
-            String line = scanner.nextLine().trim();
+        listElements.addAll(Arrays.asList(
+                scanner.nextLine().replaceAll(" ", ",").split(",")
+        ));
 
-            String[] newLine = line.split(" ");
+        int queryCount = Integer.parseInt(scanner.nextLine());
 
-            for (int i = 0; i < newLine.length; i++) {
+        for (int i = 0; i < queryCount; i++) {
+            String action = scanner.nextLine();
 
-                newList.add(Integer.parseInt(newLine[i]));
+            switch (action) {
+                case "Insert": {
+                    String[] commands = scanner.nextLine().split(" ");
+                    int index = Integer.parseInt(commands[0]);
+
+                    if (index >= listElements.size()) {
+                        while (listElements.size() < index) {
+                            listElements.add("0");
+                        }
+                        listElements.add(commands[1]);
+                    } else {
+                        listElements.set(index, commands[1]);
+                    }
+
+                    break;
+                }
+                case "Delete": {
+                    int index = Integer.parseInt(scanner.nextLine());
+                    if (index < listElements.size()) {
+                        listElements.remove(index);
+                    }
+
+                    break;
+                }
             }
-
-            lines.add(newList);
-            numberOfLines--;
         }
 
-        int queryCount = scanner.nextInt();
+        String output = "";
 
-        scanner.nextLine();
-
-        while (scanner.hasNext()) {
-            String query = scanner.nextLine();
-            String[] splitQuery = query.split(" ");
-
-
-            try {
-                System.out.println(
-                        lines
-                                .get(Integer.parseInt(splitQuery[0]) - 1)
-                                .get(Integer.parseInt(splitQuery[1]))
-                );
-            } catch (Exception err) {
-                System.out.println("ERROR!");
-            }
+        for (String element : listElements) {
+            output += element + " ";
         }
+
+        System.out.println(output.trim());
 
     }
 }
+
