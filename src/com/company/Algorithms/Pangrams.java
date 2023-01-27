cbb4a3b559acedfc50d7c33e364745c56d89141e
Matthew Greco <mgreco2112@tutanota.com>
2023-01-26 20:34:47 -0500

    refactored solution to utilize HashSet and no longer require sorting

--- src/com/company/Algorithms/Pangrams.java
+++ src/com/company/Algorithms/Pangrams.java
@@ -1,28 +1,46 @@
 package com.company.Algorithms;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Pangrams {
+//    public static String pangrams(String s) {
+//        int[] includedLetters = new int[26];
+//
+//        String caseNeutralString = s.toLowerCase();
+//
+//        for (int i = 0; i < caseNeutralString.length(); i++) {
+//            //97 - 122 && !32
+//
+//            if (caseNeutralString.charAt(i) != 32) {
+//                includedLetters[caseNeutralString.charAt(i) - 97]++;
+//            }
+//        }
+//
+//        Arrays.sort(includedLetters);
+//
+//        if (includedLetters[0] != 0) {
+//            return "pangram";
+//        } else {
+//            return "not pangram";
+//        }
+//
+//    }
+
     public static String pangrams(String s) {
-        int[] includedLetters = new int[26];
+        Set<Character> charSet = new HashSet<>();
 
-        String caseNeutralString = s.toLowerCase();
+        String lowerCaseS = s.toLowerCase();
 
-        for (int i = 0; i < caseNeutralString.length(); i++) {
-            //97 - 122 && !32
-
-            if (caseNeutralString.charAt(i) != 32) {
-                includedLetters[caseNeutralString.charAt(i) - 97]++;
-            }
+        for (int i = 0; i < lowerCaseS.length(); i++) {
+            charSet.add(lowerCaseS.charAt(i));
         }
 
-        Arrays.sort(includedLetters);
-
-        if (includedLetters[0] != 0) {
-            return "pangram";
-        } else {
+        if (charSet.size() != 27) {
             return "not pangram";
+        } else {
+            return "pangram";
         }
-
     }
 }
